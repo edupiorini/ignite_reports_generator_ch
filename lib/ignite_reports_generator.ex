dc65e06b7d921705cb4886fab2429ec2166378ff
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-16 06:53:50 -0300

    build now returns a list of lists

--- lib/ignite_reports_generator.ex
+++ lib/ignite_reports_generator.ex
@@ -1,18 +1,19 @@
 defmodule IgniteReportsGenerator do
-  @moduledoc """
-  Documentation for `IgniteReportsGenerator`.
-  """
+  def build(filename) do
+    filename
+    |> parser()
+  end
 
-  @doc """
-  Hello world.
+  defp parser(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Enum.map(fn line -> parse_line(line) end)
+  end
 
-  ## Examples
-
-      iex> IgniteReportsGenerator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(1, &String.to_integer/1)
   end
 end
