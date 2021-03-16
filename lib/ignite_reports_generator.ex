7f9182920f4812d5247f997f93fc493f45e31114
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-16 06:00:51 -0300

    init

---
+++ lib/ignite_reports_generator.ex
@@ -0,0 +1,18 @@
+defmodule IgniteReportsGenerator do
+  @moduledoc """
+  Documentation for `IgniteReportsGenerator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IgniteReportsGenerator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ignite_reports_generator_test.exs
@@ -0,0 +1,8 @@
+defmodule IgniteReportsGeneratorTest do
+  use ExUnit.Case
+  doctest IgniteReportsGenerator
+
+  test "greets the world" do
+    assert IgniteReportsGenerator.hello() == :world
+  end
+end
